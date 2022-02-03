b0addec98ea2b01250c32312469e3f71417fa6ab
MacMini-19 <macmini-19@MacMini-19s-Mac-mini.local>
2022-02-03 10:26:53 -0800

    Dropdown Menu Selection

--- test/pageobjects/form.page.js
+++ test/pageobjects/form.page.js
@@ -13,14 +13,20 @@ class FormPage extends Page {
      get inputEmail() {
           return $("#mail"); //uses CSS query selector
      }
+
+     // Error text
+    get errorText() {
+        return $('.error-text')
+    }
      
      // Submit Button Selector
      get submit() {
           return $('button=Register'); //uses Element with certain text selector
      }
+     // Get Dropdown Selector
      get interest() {
-        return $('#title option:nth-child(3)'); // //uses CSS query selector (class)
-   }
+          return $('#title option:nth-child(3)'); // //uses CSS query selector (class)
+     }
      /**
       * a method to encapsule automation code to interact with the page
       * e.g. to login using username and password
